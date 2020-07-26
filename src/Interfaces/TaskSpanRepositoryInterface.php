3fb53ac3d8bc8a1f6cfdfd4acb720f45eb546171
Karim <karim-morel@outlook.fr>
2020-07-26 23:32:35 +0200

    Updating interface

--- src/Interfaces/TaskSpanRepositoryInterface.php
+++ src/Interfaces/TaskSpanRepositoryInterface.php
@@ -4,4 +4,8 @@ namespace App\Interfaces;
 
 interface TaskSpanRepositoryInterface {
     public function getSummary();
+    public function getNewTaskInView($runningTask);
+    public function getTodaysWorkingTime();
+    public function stopRunningTaskSpan();
+    public function getRunningTaskSpan();
 }
